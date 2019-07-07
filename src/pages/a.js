a06ee9b763f0dfb87b9330a2953ab07748321b18
tovinping <244490027@qq.com>
2019-07-07 16:48:33 +0800

    添加tag

--- src/pages/a.js
+++ src/pages/a.js
@@ -1,9 +1,23 @@
 import React from 'react'
+import ErrorContainer from '../components/common/errorContainer'
+import ErrorCom from '../components/common/errorCom.js'
 
 export default class PageA extends React.Component{
+  constructor(props) {
+    super(props)
+    this.state = {
+      error: false
+    }
+  }
   render() {
+    if (this.state.error) {
+      return <h1 style={{color: 'red'}}>此组件加载失败</h1>
+    }
     return <div className="page-a">
       这是页面AAAAA
+      <ErrorContainer>
+        <ErrorCom />        
+      </ErrorContainer>
     </div>
   }
 }
